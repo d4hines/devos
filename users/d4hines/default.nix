2ff37ff972c77c4dcc0bb7ed8b9828677b8a2b9d
Daniel Hines <d4hines@gmail.com>
2021-05-23 20:05:56 +0000

    wip

---
+++ users/d4hines/default.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  home-manager.users.d4hines = { suites, ... }: {
+    imports = suites.base;
+  };
+
+  users.users.d4hines = {
+    uid = 1000;
+    hashedPassword = lib.fileContents ../../secrets/userPass;
+    description = "default";
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+}
